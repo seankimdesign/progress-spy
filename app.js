62e3b6e7d132012ce937eb8336fa73c2a23e4de7
Sean Kim <SKim@paciolan.com>
2017-12-12 15:30:11 -0800

    Improved time display

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const getMinutesDifference = (testRan) =>{
   const now = new Date()
   let difference = Math.floor(Math.abs(now - testRan) / 60000)
   let unit = 'minutes'
-  if (difference > 1440){
+  if (difference > 180){
     if (difference > 2880){
       difference = Math.floor(difference / 1440)
       unit = 'days'
