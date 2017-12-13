17d386b0d449f99995fb111f75a1a72132acce58
Sean Kim <SKim@paciolan.com>
2017-12-13 11:26:24 -0800

    Fixed bug introduced by last PR merge + Revised socket behavior to be less intrusive

--- public/script.js
+++ public/script.js
@@ -1,9 +1,52 @@
-// Vanilla JS
-const createTestBlocks = type => num => `<div class="block ${type}"></div>`.repeat(num)
+;(()=>{
 
-const createFailureBlock = failures => failures.reduce((acc, failure) => {
-  return acc + `
-    <div class="single-failure closed">
+  let openedState = {}
+
+  document.addEventListener('click', e => {
+    if (e.target.className === 'toggle') {
+      const { classList } = e.target.parentElement
+      const { dataset } = e.target.parentElement
+      if (classList.contains('open')){
+        openedState[dataset.accountId][dataset.filename] = false
+        classList.add('closed')
+        classList.remove('open')
+      } else {
+        openedState[dataset.accountId] = openedState[dataset.accountId] || {}
+        openedState[dataset.accountId][dataset.filename] = true
+        classList.remove('closed')
+        classList.add('open')
+      }
+    }
+  })
+
+  fetch('/api/trainees').then((res) => res.json()).then((data) => {
+    const pageElement = document.getElementsByClassName('page')[0]
+    pageElement.innerHTML = data.trainees.reduce((acc, trainee) => acc + buildTraineeBlock(trainee), '')
+  })
+
+  const socket = io()
+  socket.on('update', (trainee) => {
+    if (document.getElementById(trainee.id)) {
+      document.getElementById(trainee.id).outerHTML = buildTraineeBlock(trainee.testResult)
+    } else {
+      const e = document.createElement('div')
+      e.innerHTML = buildTraineeBlock(trainee.testResult, true)
+      document.getElementsByClassName('page')[0].appendChild(e.firstElementChild)
+      setTimeout(()=>{
+        const justLoaded = document.getElementsByClassName('transparent')
+        ;[].slice.call(justLoaded).forEach((elem)=> {
+          elem.classList.remove('transparent')
+        })
+      }, 0)
+    }
+  })
+
+  const createTestBlocks = type => num => `<div class="block ${type}"></div>`.repeat(num)
+
+  const createFailureBlock = (failures, id) => failures.reduce((acc, failure) => {
+    const openClosedState = openedState[id] && openedState[id][failure.fileName] ? 'open' : 'closed'
+    return acc + `
+    <div class="single-failure ${openClosedState}" data-filename="${failure.fileName}" data-account-id="${id}">
       <button class="toggle">TOGGLE</button>
       <div class="badge">${failure.details.length}</div>
       <p class="filename">${failure.fileName}</p>
@@ -18,48 +61,20 @@ const createFailureBlock = failures => failures.reduce((acc, failure) => {
       </ol>
     </div>
   `
-}, '')
+  }, '')
 
-const buildTraineeBlock = trainee => `
-  <div class="single-trainee" id="${trainee.id}">
-    <h3 class="name">${trainee.name}</h3>
-    <h5 class="subname">${trainee.subname}</h5>
-    <p class="hidden">${trainee.id}</p>
-    <p class="small">${trainee.lastRan}</p>
-    <div class="block-container">${createTestBlocks('success-block')(trainee.numPassedTestSuites)}</div>
-    <div class="block-container">${createTestBlocks('failure-block')(trainee.numFailedTestSuites)}</div>
-    ${trainee.failureDetails.length ? `<div class="fail-container">${createFailureBlock(trainee.failureDetails)}</div>` : ''}
-  </div>
-`
-
-;(()=>{
-
-  document.addEventListener('click', e => {
-    if (e.target.className === 'toggle') {
-      const { classList } = e.target.parentElement
-      if (classList.contains('open')){
-        classList.add('closed')
-        classList.remove('open')
-      } else {
-        classList.remove('closed')
-        classList.add('open')
-      }
-    }
-  })
-
-  fetch('/api/trainees').then((res) => res.json()).then((data) => {
-    document.getElementsByClassName('page')[0].innerHTML = data.trainees.reduce((acc, trainee) => acc + buildTraineeBlock(trainee), '')
-  })
-
-  const socket = io()
-  socket.on('update', (trainee) => {
-    if (document.getElementById(trainee.id)) {
-      document.getElementById(trainee.id).innerHTML = buildTraineeBlock(trainee.testResult)
-    } else {
-      var e = document.createElement('div')
-      e.innerHTML = buildTraineeBlock(trainee.testResult)
-      document.getElementsByClassName('page')[0].appendChild(e.firstElementChild)
-    }
-  })
+  const buildTraineeBlock = (trainee, shouldAnimate) => {
+    const animationClass = shouldAnimate ? 'transparent' : ''
+    return `
+      <div class="single-trainee ${animationClass}" id="${trainee.id}">
+        <h3 class="name">${trainee.name}</h3>
+        <h5 class="subname">${trainee.subname}</h5>
+        <p class="small">${trainee.lastRan}</p>
+        <div class="block-container">${createTestBlocks('success-block')(trainee.numPassedTestSuites)}</div>
+        <div class="block-container">${createTestBlocks('failure-block')(trainee.numFailedTestSuites)}</div>
+        ${trainee.failureDetails.length ? `<div class="fail-container">${createFailureBlock(trainee.failureDetails, trainee.id)}</div>` : ''}
+      </div>
+    `
+  }
 
 })()
