7f6771e68cbcc46e137d33cff9704ad46b469afe
Nicholas Lim <nlim@paciolan.com>
2017-12-28 14:38:14 -0800

    Trainees sorted by name in list

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 todo :
 readd in delete functionality
 Add in cookie or login gate
-Add in reselect to sort trainees
-reimplement speech
 reimplement transitions
--- client/src/pages/Home/HomeContainer.js
+++ client/src/pages/Home/HomeContainer.js
@@ -1,10 +1,21 @@
 import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
 import actions from '../../redux/actions'
 
 import Home from './Home'
 
-const mapStateToProps = state => ({
-  ...state
+const getTrainees = (state, props) => state.trainees || []
+
+const sortTrainees = createSelector(
+  [getTrainees],
+  (trainees) => {
+    return [...trainees].sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
+  }
+)
+
+const mapStateToProps = (state, props) => ({
+  trainees: sortTrainees(state, props)
 })
 
 export default connect(mapStateToProps, actions)(Home)
